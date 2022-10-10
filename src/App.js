ae52f1be63c3aa60be754688e32529a79e519930
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-10-10 23:09:53 +0600

    Navbar add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import logo from './logo.svg';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
 import './App.css';
+import Main from '../src/Layout/Main'
+
+import Topics from './Componenet/Topics/Topics';
+import Statistics from './Componenet/Statistics/Statistics';
+import Blog from './Componenet/Blog/Blog';
+
 
 function App() {
+     const router = createBrowserRouter(
+      [
+        {
+          path : '/',
+          element: <Main></Main>,
+          children: [
+            {
+              path : '/',
+              element: <Topics></Topics>
+            },
+            {
+             path:'/statistics',
+             element:<Statistics></Statistics>
+            },
+            {
+              path:'/blog',
+              element:<Blog></Blog>
+            }
+
+          ] 
+        }
+
+      ]
+     )
   return (
     <div className="App">
+        <RouterProvider router={router}></RouterProvider>
+
+        {/* <Home></Home> */}
      
-    </div>
+    
+     
+         </div>
   );
 }
 
---
+++ src/Componenet/Topics/Topics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Topics = () => {
+    return (
+        <div>
+            <h1>topic</h1>
+        </div>
+    );
+};
+
+export default Topics;
